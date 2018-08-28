431df11272a1f5a41743dc66ef045af57b316560
Lovro Suhadolnik <jest@lovro.rocks>
2018-08-28 12:57:26 +0200

    Correcting the list template columns

--- pushAll.sh
+++ pushAll.sh
@@ -2,14 +2,23 @@
 
 hugo
 
+echo -n "Enter the comment, if you want: "
+read cmt
+
+if [[ -z "$cmt" ]]; then
+
+	cmt="Website data update $(date +%d.%m.%Y@%H.%M)"
+	
+fi;
+
 git add -A
-git commit -m "Website data update $(date +%d.%m.%Y@%H.%M)"
+git commit -m "$cmt"
 git push github master
 
 cd public
 
 git add -A
-git commit -m "Website data update $(date +%d.%m.%Y@%H.%M)"
+git commit -m "$cmt"
 git push
 
 cd ..
